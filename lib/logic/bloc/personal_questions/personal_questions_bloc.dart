a964b463919c66918207f0f8a621a6bf561ddc25
mirkoraimo <mirko.raimo@gmail.com>
2021-03-21 21:26:18 +0100

    Added the logic to verify if the answer is correct. The user can see it into the recap page at the end of the quiz

--- lib/logic/bloc/personal_questions/personal_questions_bloc.dart
+++ lib/logic/bloc/personal_questions/personal_questions_bloc.dart
@@ -39,7 +39,7 @@ class PersonalQuestionsBloc
     } else if (event is UpdateQuestions) {
       yield* _mapUpdateQuestionsToState(event);
     } else if (event is AnswerQuestion) {
-      //TODO: Handle normal answer question
+      yield _answeringQuestion(event, state);
       if (event.index == state.personalQuestions.questions.length - 1) {
         yield PQuestionsAllAnswered(state.personalQuestions);
       }
@@ -54,6 +54,93 @@ class PersonalQuestionsBloc
       yield PersonalQuestionsLoaded(currentState.questions);
     }
   }
+
+  PersonalQuestionsState _answeringQuestion(
+      AnswerQuestion event, PersonalQuestionsState state) {
+    Question currentQuestion = state.personalQuestions.questions[event.index];
+    bool answeredCorrectly = currentQuestion.correctAnswer == event.givenAnswer;
+
+//TODO: MOVE THIS LOGIC TO THE UI
+//     print("Answed correctly: " + answeredCorrectly.toString());
+//     Scaffold.of(state.context).showSnackBar(SnackBar(
+//       backgroundColor:
+//           currentQuestion.answeredCorrectly == true ? Colors.green : Colors.red,
+//       content: new Text(
+//         textToShow,
+//         textAlign: TextAlign.center,
+//       ),
+//       duration: Duration(milliseconds: 400),
+//       behavior: SnackBarBehavior.floating,
+//       shape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.all(Radius.circular(20))),
+// //            action: new SnackBarAction(
+// //              textColor: Colors.black,
+// //              label: "OK",
+// //              onPressed: () => store.dispatch(DismissMessage()),
+// //            ),
+//     ));
+
+    if (answeredCorrectly) {
+      PersonalQuestions newPersonalQuestions = state.personalQuestions.copyWith(
+          indexesCorrectQuestions: []
+            ..addAll(state.personalQuestions.indexesCorrectQuestions)
+            ..add(event.index));
+      return new PersonalQuestionsLoaded.answerQuestion(newPersonalQuestions);
+    }
+    return state;
+  }
+
+//   QuestionsListState _answeringQuestion_old(
+//       QuestionsListState state, AnswerQuestion action) {
+//     if (state.questionsList != null &&
+//         state.questionsList.isNotEmpty &&
+//         action.currentQuestion != null) {
+//       QuestionsListState tmpState = state;
+
+//       Question currentQuestion = state.questionsList[action.currentQuestion];
+
+//       currentQuestion.answeredCorrectly =
+//           currentQuestion.correctAnswer == action.givenAnswer;
+//       String textToShow =
+//           currentQuestion.answeredCorrectly ? "$STR_RIGHT!" : "$STR_WRONG!";
+//       print(
+//           "Answed correctly: " + currentQuestion.answeredCorrectly.toString());
+//       Scaffold.of(state.context).showSnackBar(SnackBar(
+//         backgroundColor: currentQuestion.answeredCorrectly == true
+//             ? Colors.green
+//             : Colors.red,
+//         content: new Text(
+//           textToShow,
+//           textAlign: TextAlign.center,
+//         ),
+//         duration: Duration(milliseconds: 400),
+//         behavior: SnackBarBehavior.floating,
+//         shape: RoundedRectangleBorder(
+//             borderRadius: BorderRadius.all(Radius.circular(20))),
+// //            action: new SnackBarAction(
+// //              textColor: Colors.black,
+// //              label: "OK",
+// //              onPressed: () => store.dispatch(DismissMessage()),
+// //            ),
+//       ));
+
+//       if (action.currentQuestion == state.questionsList.length - 1) {
+//         tmpState = tmpState.copyWith(
+//           answeredLastQuestion: true,
+//         );
+//       }
+
+//       if (currentQuestion.answeredCorrectly == true) {
+//         tmpState = tmpState.copyWith(
+//             indexesCorrectQuestions: []
+//               ..addAll(tmpState.indexesCorrectQuestions)
+//               ..add(action.currentQuestion));
+//       }
+
+//       return tmpState;
+//     }
+//     return state;
+//   }
 
   @override
   Future<void> close() {
