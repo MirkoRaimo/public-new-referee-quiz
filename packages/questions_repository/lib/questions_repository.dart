5b7213dafce58086ef19059d693d6b631b3b8796
mirkoraimo <mirko.raimo@gmail.com>
2021-04-24 18:57:58 +0200

    Basic logic to use a local db with sembast (Repository pkg) - pt2

--- packages/questions_repository/lib/questions_repository.dart
+++ packages/questions_repository/lib/questions_repository.dart
@@ -1,5 +1,6 @@
 library questions_repository;
 
+export 'src/sembast/local_questions_repository.dart';
 export 'src/firebase_questions_repository.dart';
 export 'src/models/models.dart';
 export 'src/questions_repository.dart';
